a2770e519d984d9da06c317693acb7fe896f7457
Izar <IzarM@proton.me>
2026-03-24 10:01:10 +0000

    feat: player moves left and right

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,44 @@
 
 int	main(void)
 {
+	// Initialization
+	// -------------------------------------------------------------------
 	const int	screenWidth = 800;
 	const int	screenHeight= 450;
 
 	InitWindow(screenWidth, screenHeight, "Throwing Game Mechanics");
 
-	SetTargetFPS(60);
+	Vector2	playerPosition = { (float)screenWidth/2, 300};
+	Vector2	playerSize = { 60, 80};
 
+	SetTargetFPS(60);
+	// -------------------------------------------------------------------
+
+	// Main game loop
 	while(!WindowShouldClose())
 	{
-		//Update vars here
-		//-------------------
+		//Update 
+		//------------------------------------------------------------
+		if (IsKeyDown(KEY_RIGHT)) playerPosition.x += 4.0f;
+		if (IsKeyDown(KEY_LEFT)) playerPosition.x -= 4.0f;
+		//------------------------------------------------------------
 
-		//-------------------
 		// Draw
-		// ------------------
+		// -----------------------------------------------------------
 		BeginDrawing();
 
 			ClearBackground(RAYWHITE);
-			DrawText("Our protagonist, the humble rectangle!", 20, 20, 20, DARKGRAY);
+			DrawText("Move with [LEFT] and [RIGHT] arrow keys", 20, 20, 20, DARKGRAY);
 			
-			DrawRectangle(screenWidth/4*2 - 40, 300, 80, 100, BLACK);
+			DrawRectangleV(playerPosition, playerSize, BLACK);
 		EndDrawing();
-		// ------------------
+		// -----------------------------------------------------------
 	}
 
+	// De-Initialization
+	// -------------------------------------------------------------------
 	CloseWindow();
+	// -------------------------------------------------------------------
+	
+	return (0);
 }
